61e1d7951d771c1d3e49d7cc7acc35cc12d484e4
Łukasz Nycz <lnycz96@gmail.com>
2021-07-07 11:25:12 +0200

    Initial Commit

---
+++ Petitions(JSON)-P7/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Petitions(JSON)-P7
+//
+//  Created by Łukasz Nycz on 07/07/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
